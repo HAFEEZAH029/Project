5ae377e77da7313f656cb63a7ac2f768df8c4540
HAFEEZAH029 <khafeezah2000@gmail.com>
2025-03-11 11:28:48 -1100

    re-install react icon modules

--- project/src/Components/Footer.js
+++ project/src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import {FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa";
+import {FaInstagram, FaFacebook, FaTiktok} from 'react-icons/fa'
 
 function Footer () {
     return (
