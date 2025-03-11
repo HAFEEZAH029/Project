640e930576ab16a224cda2ed8f966a9c524287de
HAFEEZAH029 <khafeezah2000@gmail.com>
2025-03-11 02:08:59 -1100

    Reset for a fresh start

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Main from "./Components/Main";
 
 
 function App() {
   return (
     < >
-      <Header />
-      <Main />
-      <Footer />
+     
     </>
   );
 }
